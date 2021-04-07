e6a0b5633f89a23d4440e1d965040ed49bf8fa69
EC2 Default User <ec2-user@ip-172-31-26-72.ec2.internal>
2021-04-07 12:33:11 +0000

    [Finish]Rails chapter6

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # 'todolists#show'の設定を、todolistとして利用できる。
   get 'todolists/:id' => 'todolists#show', as:'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
